d3d197a71378a696a8d71f6f136a3db7f4b8b6de
Karthikeyan K <kk@krazybee.com>
2018-03-12 14:44:56 +0530

    add pdf

--- main.go
+++ main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"io/ioutil"
 	"kktestgo/controllers"
-
+	"log"
+	// "github.com/krazybee/gofpdf"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/jung-kurt/gofpdf"
 )
+
+func pdf() {
+	var pdf *gofpdf.Fpdf
+
+	pdf = gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "OK")
+	if pdf.Error() != nil {
+		log.Printf("not expecting error when rendering text")
+	}
+
+	pdf = gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.Cell(40, 10, "Not OK") // Font not set
+	if pdf.Error() == nil {
+		log.Printf("expecting error when rendering text without having set font")
+	}
+}
 
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
@@ -18,6 +39,7 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	controllers.Test()
+	pdf()
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
